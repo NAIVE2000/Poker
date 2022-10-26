c433918bc39d407852b224a77a945bf69082cc44
changhong <chcong@ucm.es>
2022-10-26 21:13:09 +0200

    cambio cartaaaaaaa

--- src/main/java/com/mycompany/pokerdefinitivo/Carta.java
+++ src/main/java/com/mycompany/pokerdefinitivo/Carta.java
@@ -5,17 +5,17 @@
 package com.mycompany.pokerdefinitivo;
 
 public class Carta {
-    private String palo;
+    private String pala;
     private String numera;
     
     public Carta(String _numero, String _palo){
         numera = _numero; 
-        palo = _palo;
+        pala = _palo;
         
     }
 
     
-    public String palo(){return palo;}
+    public String palo(){return pala;}
     public String numero(){return numera;}
     public int numInt(){return Integer.parseInt(numera);}
 }
